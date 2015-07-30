89a6d4bb7055ab7821d9bdf8e014d0804306a33b
Steeve McCauley <steeve@isentium.com>
2015-07-30 19:26:33 -0400

    * logging updates

--- sync_android_mtp.rb
+++ sync_android_mtp.rb
@@ -1,5 +1,39 @@
 #!/usr/bin/env ruby
 #
+#find_path_by_usbid () {
+#	id="$1"
+#	lsusboutput="$(lsusb | grep $id | head -n1)"
+#	usbbus="${lsusboutput% Device*}"
+#	usbbus="${usbbus#Bus }"
+#	usbdevice="${lsusboutput%%:*}"
+#	usbdevice="${usbdevice#*Device }"
+#
+#	# Media Transfer Protocol
+#	if [ -d "$XDG_RUNTIME_DIR" ]; then
+#		runtimedir="$XDG_RUNTIME_DIR"
+#	else
+#		runtimedir="/run/user/$USER"
+#	fi
+#	MtpPath="$runtimedir/gvfs/mtp:host=%5Busb%3A${usbbus}%2C${usbdevice}%5D"
+#	# Picture Transfer Protocol
+#	PtpPath="$runtimedir/gvfs/gphoto2:host=%5Busb%3A${usbbus}%2C${usbdevice}%5D"
+#
+#	if [ -d "$MtpPath" ]; then
+#		echo "$MtpPath"
+#	elif [ -d "$PtpPath" ]; then
+#		echo "$PtpPath"
+#	else
+#		echo "Error: File or directory was not found." 
+#	fi
+#}
+#
+## USB ID for Nexus 4
+#id="18d1:4ee[12]"
+#path=$(find_path_by_usbid $id)
+#
+
+#$ lsusb -d 18d1:
+#Bus 002 Device 036: ID 18d1:4ee1 Google Inc. Nexus 4
 
 require 'optparse'
 require 'logger'
@@ -9,16 +43,29 @@ require 'fileutils'
 ME=File.basename($0, ".rb")
 MD=File.dirname(File.expand_path($0))
 
+TMP="/var/tmp/#{ME}/#{ENV['USER']}"
+DST="#{TMP}/backup"
+LOG="#{TMP}/#{ME}.log"
+
 class Logger
 	def die(msg)
+		$stdout = STDOUT
 		self.error(msg)
 		exit 1
 	end
+
+	def puts(msg)
+		self.info(msg)
+	end
+
+	def write(msg)
+		self.info(msg)
+	end
 end
 
-def set_logger(stream)
+def set_logger(stream, level=Logger::INFO)
 	log = Logger.new(stream)
-	log.level = Logger::INFO
+	log.level = level
 	log.datetime_format = "%Y-%m-%d %H:%M:%S"
 	log.formatter = proc do |severity, datetime, progname, msg|
 		"#{severity} #{datetime}: #{msg}\n"
@@ -33,11 +80,12 @@ $opts = {
 	:vendor => ENV["SYNC_MTP_VENDOR"]||"18d1",
 	:product => ENV["SYNC_MTP_PRODUCT"]||"",
 	:src => "",
-	:dst => "/var/tmp/#{ME}",
+	:dst => DST,
 	:dryrun => false,
 	:verbose => false,
 	:progress => false,
-	:from => true
+	:from => true,
+	:log => nil 
 }
 
 def getSymbol(string)
@@ -79,15 +127,15 @@ def parse(gopts)
 		optparser = OptionParser.new { |opts|
 			opts.banner = "#{ME}.rb [options]"
 
-			opts.on('-d', '--dst DEST', String, "Destination directory, default #{$opts[:dst]}") { |dst|
+			opts.on('-b', '--backup DIR', String, "Backup directory, default #{$opts[:dst]}") { |dst|
 				$opts[:dst]=dst
 			}
 
-			opts.on('-f', '--from', "From mtp directory (default): #{mtp_dir}") {
+			opts.on('-f', '--from', "From mtp directory to backup: #{mtp_dir}") {
 				$opts[:from]=true
 			}
 
-			opts.on('-t', '--to', "To the mtp directory: #{mtp_dir}") {
+			opts.on('-t', '--to', "To the mtp directory from backup: #{mtp_dir}") {
 				$opts[:from]=false
 			}
 
@@ -117,13 +165,18 @@ def parse(gopts)
 				# lsusb
 				# Bus 002 Device 037: ID 18d1:4ee1 Google Inc. Nexus 4
 				out=%x/lsusb/.strip
-				puts "ID VEND:PROD Description"
+				$stdout.puts "ID VEND:PROD Description"
 				out.split(/\r?\n/).each { |line|
 					line.strip!
 					next if line[/^.*?: (.*)$/].nil?
-					puts $1
+					$stdout.puts $1
 				}
 				exit 0
+			}
+
+			opts.on('-L', '--log [FILE]', String, "Log to file, default #{$opts[:log]}") { |file|
+				$opts[:log]=file||LOG
+				$log.debug "Log file=#{$opts[:log].inspect}"
 			}
 
 			opts.on('-v', '--verbose', "Verbose output") {
@@ -135,7 +188,7 @@ def parse(gopts)
 			}
 
 			opts.on('-h', '--help', "Help") {
-				puts opts
+				$stdout.puts opts
 				exit 0
 			}
 		}
@@ -150,6 +203,15 @@ def parse(gopts)
 			$opts[:src]=dst
 			$opts[:dst]=src
 		end
+
+		unless $opts[:log].nil?
+			$log.debug "Logging file #{$opts[:log]}"
+			FileUtils.mkdir_p(File.dirname($opts[:log]))
+			$stdout=File.open($opts[:log], "a")
+			$log=set_logger($stdout, $log.level)
+			$stdout=$log
+		end
+
 	rescue OptionParser::InvalidOption => e
 		$log.die e.message
 	rescue => e
@@ -160,41 +222,6 @@ def parse(gopts)
 end
 
 $opts=parse($opts)
-
-#find_path_by_usbid () {
-#	id="$1"
-#	lsusboutput="$(lsusb | grep $id | head -n1)"
-#	usbbus="${lsusboutput% Device*}"
-#	usbbus="${usbbus#Bus }"
-#	usbdevice="${lsusboutput%%:*}"
-#	usbdevice="${usbdevice#*Device }"
-#
-#	# Media Transfer Protocol
-#	if [ -d "$XDG_RUNTIME_DIR" ]; then
-#		runtimedir="$XDG_RUNTIME_DIR"
-#	else
-#		runtimedir="/run/user/$USER"
-#	fi
-#	MtpPath="$runtimedir/gvfs/mtp:host=%5Busb%3A${usbbus}%2C${usbdevice}%5D"
-#	# Picture Transfer Protocol
-#	PtpPath="$runtimedir/gvfs/gphoto2:host=%5Busb%3A${usbbus}%2C${usbdevice}%5D"
-#
-#	if [ -d "$MtpPath" ]; then
-#		echo "$MtpPath"
-#	elif [ -d "$PtpPath" ]; then
-#		echo "$PtpPath"
-#	else
-#		echo "Error: File or directory was not found." 
-#	fi
-#}
-#
-## USB ID for Nexus 4
-#id="18d1:4ee[12]"
-#path=$(find_path_by_usbid $id)
-#
-
-#$ lsusb -d 18d1:
-#Bus 002 Device 036: ID 18d1:4ee1 Google Inc. Nexus 4
 
 def sync_blocks(fsrc, fdst, fsize, length)
 	offset=0
@@ -215,7 +242,7 @@ def sync_blocks(fsrc, fdst, fsize, length)
 		fsize  -= length
 		break if fsize <= 0
 	end
-	puts if $opts[:progress]
+	$stdout.puts if $opts[:progress]
 end
 
 def sync_file(dest, fname)
@@ -225,7 +252,7 @@ def sync_file(dest, fname)
 	dsize=File.lstat(dname).size if File.exists?(dname)
 	size_sync=fsize == dsize
 	return fsize if size_sync
-	puts "Sync #{fname}:#{fsize} -> #{dname}:#{dsize}" if $opts[:verbose]
+	$stdout.puts "Sync #{fname}:#{fsize} -> #{dname}:#{dsize}" if $opts[:verbose]
 	unless $opts[:dryrun]
 		File.open(fname, "rb") { |fsrc|
 			File.open(dname, "wb") { |fdst|
@@ -239,14 +266,14 @@ end
 def sync_dir(dest, dname)
 	toplevel=dname.chomp("/").scan(/\//).length == 0
 	ddir=File.join(dest, dname)
-	puts "Syncing directory #{ddir}" if toplevel && $opts[:verbose]
+	$stdout.puts "Syncing directory #{ddir}" if toplevel && $opts[:verbose]
 	return if File.directory?(ddir)
-	puts "Creating directory #{ddir}" unless toplevel || !$opts[:verbose]
+	$stdout.puts "Creating directory #{ddir}" unless toplevel || !$opts[:verbose]
 
 	FileUtils.mkdir_p(ddir)
 end
 
-FileUtils.mkdir_p($opts[:dst], :verbose=>$opts[:verbose])
+FileUtils.mkdir_p($opts[:dst])
 
 def sync(sdir, ddir)
 	total=0
@@ -254,7 +281,8 @@ def sync(sdir, ddir)
 	dirs=0
 	tstart=Time.new.to_i
 	FileUtils.chdir(sdir) {
-		puts "Source dir = #{sdir}" if $opts[:verbose]
+		$stdout.puts "Source dir = #{sdir}" if $opts[:verbose]
+		$stdout.puts "Backup dir = #{ddir}" if $opts[:verbose]
 		Find.find(".") { |e|
 			# strip off ./
 			e=e[2..-1]
@@ -273,7 +301,7 @@ def sync(sdir, ddir)
 	tend=Time.new.to_i-tstart
 	tend+=1 if tend==0
 	mb=total/1024/1024
-	puts "Synced #{files} files and #{dirs} dirs: #{mb} MB in #{tend} secs - #{mb/tend} MB/s"
+	$stdout.puts "Synced #{files} files and #{dirs} dirs: #{mb} MB in #{tend} secs - #{mb/tend} MB/s"
 end
 
 begin
