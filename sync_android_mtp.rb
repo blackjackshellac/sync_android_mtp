7964612f6b4b690cef7ea0873e512254607f3a3d
Steeve McCauley <steeve@oneguycoding.com>
2015-09-23 08:10:09 -0400

    Squashed commit of the following:
    commit 4583f1095e6e98ec0d14e0575f901a07319130f9
    Author: Steeve McCauley <steeve@isentium.com>
    Date:   Tue Sep 22 15:44:33 2015 -0400
    
        * --sync to remove backup files not on android device
        * can handle multiple top level directories in src
    
    commit 35f8edfae713f7ab7c09a157f7338304601b7c63
    Author: Steeve McCauley <steeve@isentium.com>
    Date:   Mon Sep 21 11:05:50 2015 -0400
    
        * delete existing destination files when teh copy from src has been skipped
        ** only when dest is not the android device
        * catch Error::IO when copying files and report error rather than failing
    
    commit 77b9d7ac6c885349ff78f6de4843041629935ea3
    Author: Steeve McCauley <steeve@oneguycoding.com>
    Date:   Fri Jul 31 07:39:33 2015 -0400
    
        * just pushing change, not quite right yet
    
    commit 89a6d4bb7055ab7821d9bdf8e014d0804306a33b
    Author: Steeve McCauley <steeve@isentium.com>
    Date:   Thu Jul 30 19:26:33 2015 -0400
    
        * logging updates

--- sync_android_mtp.rb
+++ sync_android_mtp.rb
@@ -1,24 +1,72 @@
 #!/usr/bin/env ruby
 #
+#find_path_by_usbid () {
+#	id="$1"
+#	lsusboutput="$(lsusb | grep $id | head -n1)"
+#	usbbus="${lsusboutput% Device*}"
+#	usbbus="${usbbus#Bus }"
+#	usbdevice="${lsusboutput%%:*}"
+#	usbdevice="${usbdevice#*Device }"
+#
+#	# Media Transfer Protocol
+#	if [ -d "$XDG_RUNTIME_DIR" ]; then
+#		runtimedir="$XDG_RUNTIME_DIR"
+#	else
+#		runtimedir="/run/user/$USER"
+#	fi
+#	MtpPath="$runtimedir/gvfs/mtp:host=%5Busb%3A${usbbus}%2C${usbdevice}%5D"
+#	# Picture Transfer Protocol
+#	PtpPath="$runtimedir/gvfs/gphoto2:host=%5Busb%3A${usbbus}%2C${usbdevice}%5D"
+#
+#	if [ -d "$MtpPath" ]; then
+#		echo "$MtpPath"
+#	elif [ -d "$PtpPath" ]; then
+#		echo "$PtpPath"
+#	else
+#		echo "Error: File or directory was not found."
+#	fi
+#}
+#
+## USB ID for Nexus 4
+#id="18d1:4ee[12]"
+#path=$(find_path_by_usbid $id)
+#
+
+#$ lsusb -d 18d1:
+#Bus 002 Device 036: ID 18d1:4ee1 Google Inc. Nexus 4
 
 require 'optparse'
 require 'logger'
 require 'find'
 require 'fileutils'
+require 'readline'
 
 ME=File.basename($0, ".rb")
 MD=File.dirname(File.expand_path($0))
 
+TMP="/var/tmp/#{ME}/#{ENV['USER']}"
+DST="#{TMP}/backup"
+LOG="#{TMP}/#{ME}.log"
+
 class Logger
 	def die(msg)
+		$stdout = STDOUT
 		self.error(msg)
 		exit 1
 	end
+
+	def puts(msg)
+		self.info(msg)
+	end
+
+	def write(msg)
+		self.info(msg)
+	end
 end
 
-def set_logger(stream)
+def set_logger(stream, level=Logger::INFO)
 	log = Logger.new(stream)
-	log.level = Logger::INFO
+	log.level = level
 	log.datetime_format = "%Y-%m-%d %H:%M:%S"
 	log.formatter = proc do |severity, datetime, progname, msg|
 		"#{severity} #{datetime}: #{msg}\n"
@@ -33,12 +81,21 @@ $opts = {
 	:vendor => ENV["SYNC_MTP_VENDOR"]||"18d1",
 	:product => ENV["SYNC_MTP_PRODUCT"]||"",
 	:src => "",
-	:dst => "/var/tmp/#{ME}",
+	:dst => ENV["SYNC_MTP_BACKUP"]||DST,
+	:dirs => [],
 	:dryrun => false,
 	:verbose => false,
 	:progress => false,
-	:from => true
+	:from => true,
+	:sync => false,
+	:yes => false,
+	:log => nil,
+	:delete_skipped_to => false
 }
+
+def vputs(msg, force=false)
+	$stdout.puts msg if force || $opts[:verbose]
+end
 
 def getSymbol(string)
 	string=string[1..-1] if string[0].eql?(":")
@@ -68,27 +125,51 @@ def get_mtp_directory(uid)
 	rtdir="/run/user/#{uid}/"
 	$log.die "Runtime dir not found #{rtdir}" unless File.directory?(rtdir)
 
-	mtp_dir=File.join(rtdir, "gvfs/mtp:host=%5Busb%3A#{usbbus}%2C#{usbdevice}%5D/Internal storage/")
+	mtp_dir=File.join(rtdir, "gvfs/mtp:host=%5Busb%3A#{usbbus}%2C#{usbdevice}%5D/")
 	$log.warn "mtp dir not mounted #{mtp_dir}" unless File.directory?(mtp_dir)
 	return mtp_dir
+end
+
+def get_dirs(src)
+	dirs=[]
+	FileUtils.chdir(src) {
+		Dir.glob('*') { |dir|
+			next unless File.directory?(dir)
+			dirs << dir
+		}
+	}
+	dirs
 end
 
 def parse(gopts)
 	begin
 		mtp_dir=get_mtp_directory($opts[:uid])||"device not detected"
 		optparser = OptionParser.new { |opts|
-			opts.banner = "#{ME}.rb [options]"
+			opts.banner = "#{ME}.rb [options]\n"
 
-			opts.on('-d', '--dst DEST', String, "Destination directory, default #{$opts[:dst]}") { |dst|
+			opts.on('-b', '--backup DIR', String, "Backup directory, default #{$opts[:dst]}") { |dst|
 				$opts[:dst]=dst
 			}
 
-			opts.on('-f', '--from', "From mtp directory (default): #{mtp_dir}") {
+			opts.on('-f', '--from', "From mtp directory to backup (default): #{mtp_dir}") {
 				$opts[:from]=true
 			}
 
-			opts.on('-t', '--to', "To the mtp directory: #{mtp_dir}") {
+			opts.on('-t', '--to', "To the mtp directory from backup: #{mtp_dir}") {
 				$opts[:from]=false
+			}
+
+			opts.on('-s', '--sync', "Sync from android to backup then purge old files from backup") {
+				$opts[:sync]=true
+				$opts[:delete_skipped_to]=true
+			}
+
+			opts.on('-y', '--yes', "Answer yes to prompts") {
+				$opts[:yes]=true
+			}
+
+			opts.on('-d', '--delete-skip-to', "Delete destination files when copy from src is skipped (only for --from, not --to)") {
+				$opts[:delete_skipped_to]=true
 			}
 
 			opts.on('-n', '--dry-run', "Dry run") {
@@ -117,13 +198,18 @@ def parse(gopts)
 				# lsusb
 				# Bus 002 Device 037: ID 18d1:4ee1 Google Inc. Nexus 4
 				out=%x/lsusb/.strip
-				puts "ID VEND:PROD Description"
+				$stdout.puts "ID VEND:PROD Description"
 				out.split(/\r?\n/).each { |line|
 					line.strip!
 					next if line[/^.*?: (.*)$/].nil?
-					puts $1
+					$stdout.puts $1
 				}
 				exit 0
+			}
+
+			opts.on('-L', '--log [FILE]', String, "Log to file instead of stdout, default #{$opts[:log]}") { |file|
+				$opts[:log]=file||LOG
+				$log.info "Logging to #{$opts[:log].inspect}"
 			}
 
 			opts.on('-v', '--verbose', "Verbose output") {
@@ -135,21 +221,48 @@ def parse(gopts)
 			}
 
 			opts.on('-h', '--help', "Help") {
-				puts opts
+				$stdout.puts ""
+				$stdout.puts opts
+				$stdout.puts "\nEnvironment variables:\n"
+				$stdout.puts "\tSYNC_MTP_VENDOR  - usb vendor code\n"
+				$stdout.puts "\tSYNC_MTP_PRODUCT - usb product code\n"
+				$stdout.puts "\tSYNC_MTP_BACKUP  - default backup directory\n"
 				exit 0
 			}
 		}
 		optparser.parse!
 
+		if $opts[:sync]
+			$log.die "Cannot use --to with --sync" unless $opts[:from]
+			$opts[:from]=true
+			$opts[:record]=true
+			$opts[:delete_skipped_to]=true
+		end
 		src=get_mtp_directory($opts[:uid])
 		dst=$opts[:dst]
 		if $opts[:from]
 			$opts[:src]=src
 			$opts[:dst]=dst
 		else
+			$log.error "Resetting --delete_skipped_to=false" if $opts[:delete_skipped_to]
+			$opts[:delete_skipped_to]=false
+
 			$opts[:src]=dst
 			$opts[:dst]=src
 		end
+
+		$opts[:dirs]=get_dirs(src)
+
+		unless $opts[:log].nil?
+			$log.debug "Logging file #{$opts[:log]}"
+			FileUtils.mkdir_p(File.dirname($opts[:log]))
+			# open log to $stdout
+			$stdout=File.open($opts[:log], "a")
+			# create a logger pointing to stdout
+			$log=set_logger($stdout, $log.level)
+			$stdout=$log
+		end
+
 	rescue OptionParser::InvalidOption => e
 		$log.die e.message
 	rescue => e
@@ -160,41 +273,6 @@ def parse(gopts)
 end
 
 $opts=parse($opts)
-
-#find_path_by_usbid () {
-#	id="$1"
-#	lsusboutput="$(lsusb | grep $id | head -n1)"
-#	usbbus="${lsusboutput% Device*}"
-#	usbbus="${usbbus#Bus }"
-#	usbdevice="${lsusboutput%%:*}"
-#	usbdevice="${usbdevice#*Device }"
-#
-#	# Media Transfer Protocol
-#	if [ -d "$XDG_RUNTIME_DIR" ]; then
-#		runtimedir="$XDG_RUNTIME_DIR"
-#	else
-#		runtimedir="/run/user/$USER"
-#	fi
-#	MtpPath="$runtimedir/gvfs/mtp:host=%5Busb%3A${usbbus}%2C${usbdevice}%5D"
-#	# Picture Transfer Protocol
-#	PtpPath="$runtimedir/gvfs/gphoto2:host=%5Busb%3A${usbbus}%2C${usbdevice}%5D"
-#
-#	if [ -d "$MtpPath" ]; then
-#		echo "$MtpPath"
-#	elif [ -d "$PtpPath" ]; then
-#		echo "$PtpPath"
-#	else
-#		echo "Error: File or directory was not found." 
-#	fi
-#}
-#
-## USB ID for Nexus 4
-#id="18d1:4ee[12]"
-#path=$(find_path_by_usbid $id)
-#
-
-#$ lsusb -d 18d1:
-#Bus 002 Device 036: ID 18d1:4ee1 Google Inc. Nexus 4
 
 def sync_blocks(fsrc, fdst, fsize, length)
 	offset=0
@@ -215,7 +293,29 @@ def sync_blocks(fsrc, fdst, fsize, length)
 		fsize  -= length
 		break if fsize <= 0
 	end
-	puts if $opts[:progress]
+	$stdout.puts "" if $opts[:progress]
+end
+
+def ask_yes_no_all(prompt)
+	prompt+= " [y/N/all] $ "
+	return true if $opts[:yes]
+	line=Readline.readline(prompt).strip
+	unless line[/all/i].nil?
+		$opts[:yes]=true
+		return true
+	end
+	return line[/(y|yes)/i].nil? ? false : true
+end
+
+def sync_delete(dest, fname)
+	dname=File.join(dest, fname)
+	found=File.exist?(dname)
+	return unless found
+	if ask_yes_no_all("Delete #{dname}")
+		$log.debug "Deleting destination #{dname}"
+		opts={ :verbose => $opts[:verbose] }
+		FileUtils.rm_f(dname, opts) unless $opts[:dryrun]
+	end
 end
 
 def sync_file(dest, fname)
@@ -224,47 +324,74 @@ def sync_file(dest, fname)
 	dsize=-1
 	dsize=File.lstat(dname).size if File.exists?(dname)
 	size_sync=fsize == dsize
+	# size is the same, assume files are synced
 	return fsize if size_sync
-	puts "Sync #{fname}:#{fsize} -> #{dname}:#{dsize}" if $opts[:verbose]
-	unless $opts[:dryrun]
+	vputs "Sync #{fname}:#{fsize} -> #{dname}:#{dsize}"
+	return fsize if $opts[:dryrun]
+	begin
 		File.open(fname, "rb") { |fsrc|
 			File.open(dname, "wb") { |fdst|
 				sync_blocks(fsrc, fdst, fsize, 1024*1024)
 			}
 		}
+		return fsize
+	rescue Errno::EIO => e
+		$log.error "Failed to sync #{fname} to #{dname}"
 	end
-	return fsize
+	return 0
 end
 
 def sync_dir(dest, dname)
 	toplevel=dname.chomp("/").scan(/\//).length == 0
 	ddir=File.join(dest, dname)
-	puts "Syncing directory #{ddir}" if toplevel && $opts[:verbose]
+	$log.info "Syncing to #{ddir}" if toplevel && $opts[:verbose]
 	return if File.directory?(ddir)
-	puts "Creating directory #{ddir}" unless toplevel || !$opts[:verbose]
-
+	$log.info "Creating directory #{ddir}" unless toplevel || !$opts[:verbose]
 	FileUtils.mkdir_p(ddir)
 end
 
-FileUtils.mkdir_p($opts[:dst], :verbose=>$opts[:verbose])
+RE_ANDROID_DATA_CACHE=/^Android\/data\/.*?\/cache\//i
+RE_THUMBNAILS=/(^|\/).thumbnails\//i
+RE_SKIP_ARRAY = [ RE_ANDROID_DATA_CACHE, RE_THUMBNAILS ]
+def skip_path(path)
+	skip=false
+	# /^Android\/data\/.*\/cache\//i
+	RE_SKIP_ARRAY.each { |re|
+		m=re.match(path)
+		next if m.nil?
+		$log.debug "Skipping path #{path}: #{re.to_s}" if $opts[:verbose]
+		skip=true
+		break
+	}
+	return skip
+end
 
-def sync(sdir, ddir)
+def sync(sdir, ddir, record=nil)
+	skip = false
 	total=0
 	files=0
 	dirs=0
 	tstart=Time.new.to_i
 	FileUtils.chdir(sdir) {
-		puts "Source dir = #{sdir}" if $opts[:verbose]
+		vputs "Source dir = #{sdir}"
+		vputs "Backup dir = #{ddir}"
 		Find.find(".") { |e|
 			# strip off ./
 			e=e[2..-1]
 			next if e.nil?
+			skip=skip_path(e)
 			if File.directory?(e)
+				next if skip
 				dirs+=1
 				sync_dir(ddir, e)
 			elsif File.file?(e)
+				if skip
+					sync_delete(ddir, e) if $opts[:delete_skipped_to]
+					next
+				end
 				files+=1
 				total+=sync_file(ddir, e)
+				record[e]=true unless record.nil?
 			else
 				$log.warn "Skipping file #{e}: #{File.lstat(e).inspect}"
 			end
@@ -273,15 +400,55 @@ def sync(sdir, ddir)
 	tend=Time.new.to_i-tstart
 	tend+=1 if tend==0
 	mb=total/1024/1024
-	puts "Synced #{files} files and #{dirs} dirs: #{mb} MB in #{tend} secs - #{mb/tend} MB/s"
+	vputs("Synced #{files} files and #{dirs} dirs: #{mb} MB in #{tend} secs - #{mb/tend} MB/s", true)
+end
+
+def sync_toplevel(toplevel)
+	src=File.join($opts[:src], toplevel)
+	dst=File.join($opts[:dst], toplevel)
+
+	$log.info "Backup #{src} to #{dst}"
+
+	record = $opts[:sync] ? {} : nil
+	FileUtils.mkdir_p(dst)
+	sync(src, dst, record)
+	if $opts[:sync]
+		$log.info "Recorded #{record.size} files in sync from"
+		# delete files in dst that were not recorded during first sync
+		FileUtils.chdir(dst) {
+			vputs "Working dir = #{dst}"
+			Find.find(".") { |e|
+				# strip off ./
+				e=e[2..-1]
+				next if e.nil?
+				next unless File.file?(e)
+				next if record.key?(e)
+				vputs("Found file not recorded in src: #{e}",true)
+				sync_delete(dst, e)
+			}
+		}
+		src,dst=dst,src
+		$opts[:delete_skipped_to]=false
+		$opts[:from]=false
+		#sync(src, dst)
+	end
 end
 
 begin
-	sync($opts[:src], $opts[:dst])
+	$log.die "No dirs found in #{$opts[:src]}" if $opts[:dirs].empty?
+	$opts[:dirs].each { |toplevel|
+		sync_toplevel(toplevel)
+	}
 rescue Errno::EIO => e
 	$log.die "Quitting on IO error: #{e.message}"
 rescue Errno::ENOENT => e
 	$log.die e.message
 rescue Interrupt => e
 	$log.die "Interrupted"
+rescue => e
+	e.backtrace.each { |tr|
+		puts tr
+	}
+	$log.die e.message
 end
+
